58a8fa26c58a34b3c69665cdf952070e5ec5dee9
BMustico <musticbt@clarkson.edu>
2022-09-01 09:40:38 -0400

    Test Commit 1

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * A simple Hello World program
+ *
+ * @author Benjamin T. Mustico
+ * @version 31 August, 2022
+ */
+
+public class HelloWorld
+{
+    public void sayHello()
+    {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main
+{
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+
+        hello.sayHello();
+    }
+}
